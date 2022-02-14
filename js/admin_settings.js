a3c4d3a5079deaf24fba7d3caa22ff4cb9d8d4a9
unknown <alaaosama2121998@gmail.com>
2022-02-15 01:08:10 +0200

    all page

--- js/admin_settings.js
+++ js/admin_settings.js
@@ -2,13 +2,17 @@ const chngButton = document.getElementById("change-bt");
 const txts = document.getElementsByClassName("in3");
 var counter = true;
 
-txts.forEach((txt)=>{
-    if(txt.value === ""){
-        counter = false;
+
+
+chngButton.addEventListener("click",() =>{
+    for (const txt of txts) {
+        if(txt.value === ""){
+            counter = false;
+        }
     }
-})
-if(counter === true){
-    chngButton.addEventListener("click",() =>{
+    if(counter === true){
         alert("Data Changed Successfully");
-    })
-}
+    }
+   
+})
+
