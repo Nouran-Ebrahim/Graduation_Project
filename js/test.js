e36d9796b1302fc2c2def8492e9c2a19442bfb20
fizards12 <mahmoudsameh734@gmail.com>
2022-02-16 21:27:52 +0200

    page not finished

--- js/test.js
+++ js/test.js
@@ -1,48 +1,47 @@
-$(document).ready(function(){
-  $(".R").click(function(){
-    $(".R").removeClass("active");
-    $(this).addClass("active");
-  });
-  $(".btns").click(function(){
-    $(".btns").removeClass("active");
-    $(this).addClass("active");
-  });
+
+$(".R").click(function () {
+  $(".R").removeClass("active");
+  $(this).addClass("active");
+});
+$(".btns").click(function () {
+  $(".btns").removeClass("active");
+  $(this).addClass("active");
+});
 
 
 
 
-  $(".filters").on("keyup",function(){
-    let text = $(this).val().toLowerCase();
-    $(".btns-container button").each(function(){
-      if($(this).text().toLowerCase().search(text) > -1){
-        $(this).show();
-      } else{
-        $(this).hide();
-      }
-    });
-  });
-
-  
-  
-  
-  
-  
-  const canvToNav = ()=>{
-    if($(window).width() < 768){
-      if($("#navigation").hasClass("navbar")){
-        $("#navigation").toggleClass("navbar navbar-nav navbar-light Radius offcanvas offcanvas-start")
-      }
+$(".filters").on("keyup", function () {
+  let text = $(this).val().toLowerCase();
+  $(".btns-container button").each(function () {
+    if ($(this).text().toLowerCase().search(text) > -1) {
+      $(this).show();
+    } else {
+      $(this).hide();
     }
-    else{
-      if($("#navigation").hasClass("offcanvas")){
-        $("#navigation")
-        .toggleClass("navbar navbar-nav navbar-light Radius offcanvas offcanvas-start")
-        .removeAttr('style');
-      }
+  });
+});
+
+
+
+
+
+
+const canvToNav = () => {
+  if ($(window).width() < 768) {
+    if ($("#navigation").hasClass("navbar")) {
+      $("#navigation").toggleClass("navbar navbar-nav navbar-light Radius offcanvas offcanvas-start")
     }
   }
+  else {
+    if ($("#navigation").hasClass("offcanvas")) {
+      $("#navigation")
+        .toggleClass("navbar navbar-nav navbar-light Radius offcanvas offcanvas-start")
+        .removeAttr('style');
+    }
+  }
+}
 
-  canvToNav();
+canvToNav();
 
-  $(window).resize(canvToNav)
-})
+$(window).resize(canvToNav)
