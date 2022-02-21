60674cb0f080a747ec8d8005555a41c2700549eb
unknown <nouran.ssp@gmail.com>
2022-02-22 00:03:02 +0200

    seee

--- pages/admin_settings.php
+++ pages/admin_settings.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+if (isset($_SESSION["user"]) == false) {
+  header("location:login.php");
+  exit();
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -24,13 +31,25 @@
       </div>
       <div class="change-data">
         <p class="sett ">Admin settings</p>
-        <form method="get" id="form3" name="myForm" action="../pages/login.html">
+        <form method="post" id="form3" name="myForm" action="../pages/login.html">
           <label style="display: block;" class="lab3">Admin Name:</label>
           <input type="text" name="Adminname" placeholder="admin name " class="in3" required autocomplete="off">
           <label style="display: block;" class="lab3"> Old Password:</label>
           <input type="password" name="pass" placeholder="old password" class="in3" id="pass" required>
           <label style="display: block;" class="lab3"> New Password:</label>
           <input type="password" name="npass" placeholder="new password" class="in3" id="npass" required>
+          <?php
+          if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if ($_SESSION["user"] === $_POST['Adminname'] && $_SESSION["pass"] === $_POST['pass']) {
+              //$oldPassword = $conn->query("SELECT Admin_Password FROM admin_info ")->fetchAll(pdo::FETCH_COLUMN);  //one COLUMNS 
+              //$replace = array_replace($oldPassword, $_POST['pass']);
+              echo "data is right";
+            }
+            else{
+              echo "data is false";
+            }
+          }
+          ?>
           <input type="submit" name="change" id="change-bt" value="Change">
         </form>
       </div>
