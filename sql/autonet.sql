99a047d9f82f0a5a8bd98b74aebb9ec745eb0d43
unknown <nouran.ssp@gmail.com>
2022-02-28 20:53:52 +0200

    hhhh

--- sql/autonet.sql
+++ sql/autonet.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 21, 2022 at 11:18 PM
+-- Generation Time: Feb 28, 2022 at 01:31 PM
 -- Server version: 10.4.16-MariaDB
 -- PHP Version: 7.4.12
 
@@ -40,6 +40,50 @@ CREATE TABLE `admin_info` (
 INSERT INTO `admin_info` (`id`, `Admin_Name`, `Admin_Password`) VALUES
 (1, 'admin', 'autonet');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `routers`
+--
+
+CREATE TABLE `routers` (
+  `id` int(11) NOT NULL,
+  `Hostname` varchar(100) NOT NULL,
+  `Enable_pass` varchar(100) NOT NULL,
+  `Welcom_mess` text NOT NULL,
+  `Console_pass` varchar(100) NOT NULL,
+  `Telnet_pass` varchar(100) NOT NULL,
+  `Interface_loopback` varchar(100) NOT NULL,
+  `Mask` varchar(100) NOT NULL,
+  `Ip` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `routers`
+--
+
+INSERT INTO `routers` (`id`, `Hostname`, `Enable_pass`, `Welcom_mess`, `Console_pass`, `Telnet_pass`, `Interface_loopback`, `Mask`, `Ip`) VALUES
+(1, 'router1', '1.25.22', 'good mooring', '2.258.11', '125.588', '12.558', '1.25814', '1.258.2558'),
+(2, 'router2', '1.255882.5', 'hiii', '101448.2558', '1.255.2255', '1.22552', '1222582', '115522');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `switches`
+--
+
+CREATE TABLE `switches` (
+  `id` int(11) NOT NULL,
+  `Hostname` varchar(100) NOT NULL,
+  `Enable_pass` varchar(100) NOT NULL,
+  `Welcom_mess` text NOT NULL,
+  `Console_pass` varchar(100) NOT NULL,
+  `Telnet_pass` varchar(100) NOT NULL,
+  `Interface_vlan` varchar(100) NOT NULL,
+  `Mask` varchar(100) NOT NULL,
+  `Ip` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 --
 -- Indexes for dumped tables
 --
@@ -51,6 +95,18 @@ ALTER TABLE `admin_info`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `routers`
+--
+ALTER TABLE `routers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `switches`
+--
+ALTER TABLE `switches`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -59,6 +115,18 @@ ALTER TABLE `admin_info`
 --
 ALTER TABLE `admin_info`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `routers`
+--
+ALTER TABLE `routers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `switches`
+--
+ALTER TABLE `switches`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
