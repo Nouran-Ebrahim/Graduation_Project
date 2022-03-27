cdb3417a15891b2f4e46f8b826ac11fca0d3e292
unknown <alaaosama2121998@gmail.com>
2022-03-27 21:25:00 +0200

    initial settings

--- pages/addrouter.php
+++ pages/addrouter.php
@@ -39,6 +39,11 @@ session_start();
                          $name="../network_data/uptade_routers_scripts/router".$id.".py";
                         file_put_contents($name, $myfile2); 
                         echo "<script>alert('Data changed successfully')</script>";
+                        // run python script
+                        $pythonName="python ".$name;
+                        $command = escapeshellcmd($pythonName);
+                        $output = shell_exec($command);
+                        echo "<script>alert('Run GNS successfully')</script>";
                         header("REFRESH:0.2;URL=setting_page.php");
                       }
                       else {
