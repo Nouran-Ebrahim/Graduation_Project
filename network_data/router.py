061e8081139785476d8f20c22db907a0c852d4f6
your_name <your_email_id>
2022-03-31 22:16:19 +0200

    unfinished

--- network_data/router.py
+++ network_data/router.py
@@ -1,41 +1,70 @@
-#1st part
-
 import telnetlib
+import time
+Welcomingmessage="hello to Electrical_Building"
+Interfaceloopback="1"
+Mask="255.255.255.0"
+Ip = "20.10.10.2" # New Ip
+ 
+ip = "10.10.10.2" # Old Ip
 
-Hostname=  "host_name" 
-Enable=  "enable_e" 
-Welcomingmessage=  "welcoming_message"
-Console="consle_e"
-TelnetPassword =  "pass_word"
-Interfaceloopback="numof_loopback"
-Mask="ma_sk"
-Ip = "i_p"
-tn = telnetlib.Telnet(Ip)
-tn.read_until(b"Password : ")
-tn.write(TelnetPassword.encode('ascii') + b"\n")
+telnetpw="cisco"  # Old Telnet Password
 
-#configration
+TelnetPassword="cisco" # New Telnet Password
 
-tn.write(b"enable\n")
-tn.write(Enable.encode('ascii') + b"\n")
-tn.write(b"conf t\n")
-tn.write(b"Line console 0\n")
-tn.write(b"Password \t")
+Console="cisco"
+Hostname="Electrical_Building"
+
+enablepass="cisco" # Old Telnet Password
+
+enablepassword="cisco" # New Telnet Password
+
+tn = telnetlib.Telnet(ip)
+time.sleep(2)
+response = tn.read_until(b"Password:", 5)
+
+if b"Password" in response:
+	print("found")
+	tn.write(telnetpw.encode('ascii') + b"\n")
+	output = tn.read_until(b":", 5)
+	print(output)
+	
+	
+else:
+	print("not found")
+ 
+	
+tn.write(b"enable"+b"\n")
+tn.write(enablepass.encode('ascii') + b"\n")
+tn.write(b"conf t"+b"\n")
+tn.write(b"enable secret ")
+tn.write(enablepassword.encode('ascii') + b"\n")
+tn.write(b"Line console 0"+b"\n")
+tn.write(b"Password ")
 tn.write(Console.encode('ascii') + b"\n")
-tn.write(b"Login\n")
-tn.write(b"exit \n")
-tn.write(b"conf t\n")
-tn.write(b"hostname\t")
+tn.write(b"Login"+b"\n")
+tn.write(b"exit"+b"\n")
+tn.write(b"hostname ")
 tn.write(Hostname.encode('ascii') + b"\n")
-tn.write(b"banner motd \t")
-tn.write(Welcomingmessage.encode('ascii') + b"\n")
-tn.write(b"exit \n")
-tn.write(b"conf t\n")
-tn.write(b"interface loopback\t")
+tn.write(b"Line vty 0 15"+b"\n")
+tn.write(b"Password ")
+tn.write(TelnetPassword.encode('ascii') + b"\n")
+tn.write(b"Login"+b"\n")
+tn.write(b"exit"+b"\n")
+tn.write(b"banner motd #")
+tn.write(Welcomingmessage.encode('ascii') + b"#\n")
+tn.write(b"interface loopback ")
 tn.write(Interfaceloopback.encode('ascii') + b"\n")
-tn.write(b"no shutdown\n")
-tn.write(b"ip addresse\t")
-tn.write(Ip.encode('ascii') + b"\t")
+tn.write(b"no shutdown"+b"\n")
+tn.write(b"Ip address ")
+tn.write(Ip.encode('ascii') + b" ")
 tn.write(Mask.encode('ascii') + b"\n")
-tn.write(b"exit \n")
-print("done")
+tn.write(b"exit"+b"\n")
+tn.write(b"exit"+b"\n")
+tn.write(b"wr"+b"\n\n")
+tn.write(b"exit"+b"\n")
+
+
+output = tn.read_until(b'[Connection to host lost]')
+
+print(output)
+tn.close()
--- network_data/switch.py
+++ network_data/switch.py
@@ -1,40 +1,78 @@
-#1st part
-
 import telnetlib
-Hostname= "host_name" 
-Enable= "enable_e"
-Welcomingmessage= "welcoming_message"
-Console="consle_e"
-TelnetPassword =   "pass_word"
-Interfacevlan="numof_loopback"
-Mask="ma_sk"
-Ip = "i_p"
-tn = telnetlib.Telnet(Ip)
-tn.read_until(b"Password : ")
-tn.write(TelnetPassword.encode('ascii') + b"\n")
+import time
+Welcomingmessage="hello to 3rd-floor"
+Interfacevlan="1"
+Mask="255.255.255.0"
+ip = "10.10.10.2" #DataBase Router
 
-#configration
+Ip= "14.1.1.2" # New Ip Vlan 
 
-tn.write(b"enable\n")
-tn.write(Enable.encode('ascii') + b"\n")
-tn.write(b"conf t\n")
-tn.write(b"Line console 0\n")
-tn.write(b"Password \t")
+telnetpw="cisco"
+
+Console="cisco"
+
+Hostname="3rd-floor"
+
+TelnetPassword="cisco"
+
+enable_router="cisco"
+
+enable_switch="cisco"
+
+switch_ip="14.1.1.2" # the Current Ip Vlan
+
+switch_telnetpw="cisco"
+
+tn = telnetlib.Telnet(ip)
+time.sleep(2)
+response = tn.read_until(b"Password:", 5)
+#enablepassrouter2="cisco"
+#enablepassrouter=enablepassrouter2
+if b"Password" in response:
+	print("found")
+	tn.write(telnetpw.encode('ascii') + b"\n")
+	output = tn.read_until(b":", 5)
+	print(output)
+	
+	
+else:
+	print("not found") 
+tn.write(b"enable"+b"\n")
+tn.write(enable_router.encode('ascii') + b"\n")
+tn.write(b"telnet ")
+tn.write(switch_ip.encode('ascii') + b"\n")  #database
+tn.write(switch_telnetpw.encode('ascii') + b"\n") #database
+tn.write(b"enable"+b"\n")
+tn.write(enable_switch.encode('ascii') + b"\n") #database
+tn.write(b"conf t"+b"\n")
+tn.write(b"Line console 0"+b"\n")
+tn.write(b"Password ")
 tn.write(Console.encode('ascii') + b"\n")
-tn.write(b"Login\n")
-tn.write(b"exit \n")
-tn.write(b"conf t\n")
-tn.write(b"conf t\n")
-tn.write(b"hostname\t")
+tn.write(b"Login"+b"\n")
+tn.write(b"exit"+b"\n")
+tn.write(b"hostname ")
 tn.write(Hostname.encode('ascii') + b"\n")
-tn.write(b"banner motd \t")
-tn.write(Welcomingmessage.encode('ascii') + b"\n")
-tn.write(b"exit \n")
-tn.write(b"conf t\n")
-tn.write(b"interface vlan\t")
+tn.write(b"Line vty 0 15"+b"\n")
+tn.write(b"Password ")
+tn.write(TelnetPassword.encode('ascii') + b"\n")
+tn.write(b"Login"+b"\n")
+tn.write(b"exit"+b"\n")
+tn.write(b"banner motd #")
+tn.write(Welcomingmessage.encode('ascii') + b"#\n")
+tn.write(b"interface vlan ")
 tn.write(Interfacevlan.encode('ascii') + b"\n")
-tn.write(b"no shutdown\n")
-tn.write(b"ip addresse\t")
-tn.write(Ip.encode('ascii') + b"\t")
+tn.write(b"no shutdown"+b"\n")
+tn.write(b"Ip address ")
+tn.write(Ip.encode('ascii') + b" ")
 tn.write(Mask.encode('ascii') + b"\n")
-tn.write(b"exit \n")
+tn.write(b"exit"+b"\n")
+tn.write(b"exit"+b"\n")
+tn.write(b"wr"+b"\n\n")
+tn.write(b"exit"+b"\n")
+
+
+output = tn.read_until(b'closed by foreign host]')
+
+
+print(output)
+tn.close()
