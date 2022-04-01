9fe2f8b881c428c1b434f153835f95c403149784
fizards12 <mahmoudsameh734@gmail.com>
2022-04-01 11:49:30 +0200

    unfinished

--- network_data/router.py
+++ network_data/router.py
@@ -1,22 +1,23 @@
 import telnetlib
 import time
-Welcomingmessage="welcoming_message"
+Welcomingmessage="welcoming_message" #user
 Interfaceloopback="numof_loopback"
-Mask="ma_sk"
-Ip = "i_p" # New Ip
+Mask="ma_sk" #user
  
-ip = "ip_old" # Old Ip
+Ip = "i_p" # New Ip interface
+ 
+ip = "ip_old" # Old Ip database
 
-telnetpw="tel_old"  # Old Telnet Password
+telnetpw="tel_old"  # Old Telnet Password database
 
-TelnetPassword="pass_word" # New Telnet Password
+TelnetPassword="pass_word" # New Telnet Password user
 
-Console="consle_e"
-Hostname="host_name"
+Console="consle_e"  #user
+Hostname="host_name" #user
 
-enablepass="en_old" # Old Telnet Password
+enablepass="en_old" # Old Telnet Password database
 
-enablepassword="enable_e" # New Telnet Password
+enablepassword="enable_e" # New Telnet Password user
 
 tn = telnetlib.Telnet(ip)
 time.sleep(2)
@@ -35,31 +36,44 @@ else:
 	
 tn.write(b"enable"+b"\n")
 tn.write(enablepass.encode('ascii') + b"\n")
+#change enable password
+
 tn.write(b"conf t"+b"\n")
-tn.write(b"enable secret ")
+tn.write(b"enable password ")
 tn.write(enablepassword.encode('ascii') + b"\n")
+#change console password
+
 tn.write(b"Line console 0"+b"\n")
 tn.write(b"Password ")
 tn.write(Console.encode('ascii') + b"\n")
 tn.write(b"Login"+b"\n")
 tn.write(b"exit"+b"\n")
+#change hostname
+
 tn.write(b"hostname ")
 tn.write(Hostname.encode('ascii') + b"\n")
+#change telnet password
+
 tn.write(b"Line vty 0 15"+b"\n")
 tn.write(b"Password ")
 tn.write(TelnetPassword.encode('ascii') + b"\n")
 tn.write(b"Login"+b"\n")
 tn.write(b"exit"+b"\n")
+#change welcoming message
+
 tn.write(b"banner motd #")
 tn.write(Welcomingmessage.encode('ascii') + b"#\n")
-tn.write(b"interface loopback ")
-tn.write(Interfaceloopback.encode('ascii') + b"\n")
+#change ip of interface 3/0
+
+tn.write(b"interface FastEthernet 3/0 "+b"\n")
 tn.write(b"no shutdown"+b"\n")
 tn.write(b"Ip address ")
 tn.write(Ip.encode('ascii') + b" ")
 tn.write(Mask.encode('ascii') + b"\n")
 tn.write(b"exit"+b"\n")
 tn.write(b"exit"+b"\n")
+#saving configration
+
 tn.write(b"wr"+b"\n\n")
 tn.write(b"exit"+b"\n")
 
--- network_data/switch.py
+++ network_data/switch.py
@@ -1,25 +1,25 @@
 import telnetlib
 import time
-Welcomingmessage="welcoming_message"
-Interfacevlan="numof_vlan"
-Mask="ma_sk"
+Welcomingmessage="welcoming_message" #user
+Interfacevlan="numof_vlan" #user
+Mask="ma_sk" #user
 ip = "router_ip" #DataBase Router
+switch_ip="sw_ip" # the Current Ip Vlan (old ip switch) database
+Ip= "i_p" # New Ip Vlan (switch)#user
 
-Ip= "i_p" # New Ip Vlan (switch)
+telnetpw="r_tel"  # telnet router  database
+switch_telnetpw="sw_tel" # old telnet switch database
+TelnetPassword="pass_word" # new telnet switch #user
 
-telnetpw="r_tel"  # telnet router 
-switch_telnetpw="sw_tel" # old telnet switch 
-TelnetPassword="pass_word" # new telnet switch 
+Console="consle_e" #user
 
-Console="consle_e"
+Hostname="host_name" #user
+ 
+enable_router="en_router" #  old enable router database
+enable_switch="sw_en"  # old enable switch database
+enablepassword = "enable_e" # new enable switch #user
 
-Hostname="host_name"
 
-enable_router="en_router" #  enable router
-enable_switch="sw_en"  # old enable switch
-enablepassword = "enable_e" # new enable switch
-
-switch_ip="sw_ip" # the Current Ip Vlan (old ip switch)
 
 
 
@@ -44,23 +44,35 @@ tn.write(switch_ip.encode('ascii') + b"\n")  #database
 tn.write(switch_telnetpw.encode('ascii') + b"\n") #database
 tn.write(b"enable"+b"\n")
 tn.write(enable_switch.encode('ascii') + b"\n") #database
+
+#change enable password
 tn.write(b"conf t"+b"\n")
-tn.write(b"enable secret ")
+tn.write(b"enable password ")
 tn.write(enablepassword.encode('ascii') + b"\n")
+#change console password
+
 tn.write(b"Line console 0"+b"\n")
 tn.write(b"Password ")
 tn.write(Console.encode('ascii') + b"\n")
 tn.write(b"Login"+b"\n")
 tn.write(b"exit"+b"\n")
+#change hostname
+
 tn.write(b"hostname ")
 tn.write(Hostname.encode('ascii') + b"\n")
+#change telnet password
+
 tn.write(b"Line vty 0 15"+b"\n")
 tn.write(b"Password ")
 tn.write(TelnetPassword.encode('ascii') + b"\n")
 tn.write(b"Login"+b"\n")
 tn.write(b"exit"+b"\n")
+#change welcoming message
+
 tn.write(b"banner motd #")
 tn.write(Welcomingmessage.encode('ascii') + b"#\n")
+#change ip of vlan
+
 tn.write(b"interface vlan ")
 tn.write(Interfacevlan.encode('ascii') + b"\n")
 tn.write(b"no shutdown"+b"\n")
@@ -69,6 +81,8 @@ tn.write(Ip.encode('ascii') + b" ")
 tn.write(Mask.encode('ascii') + b"\n")
 tn.write(b"exit"+b"\n")
 tn.write(b"exit"+b"\n")
+#saving configration
+
 tn.write(b"wr"+b"\n\n")
 tn.write(b"exit"+b"\n")
 
